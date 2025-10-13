58e3321b594373e1c9cb8af885be77e3489b17ba
KC-2001MS <iroiro.work1234@gmail.com>
2025-10-13 15:16:04 +0900

    fix error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
+  // trailingSlash: true,
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
 };
 
 export default nextConfig;
